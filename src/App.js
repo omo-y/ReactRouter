d19a27bbe4224a441b2e7eb291956a05728aaa1d
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-23 13:39:37 +0900

    router20210723002

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import { HOME } from "./HOME";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import "./styles.css";
 
 export default function App() {
@@ -20,9 +22,22 @@ export default function App() {
         <Route exact path="/">
           <HOME />
         </Route>
-        <Route path="/Page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/Page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/DetailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/DetailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
         <Route path="/Page2">
           <Page2 />
         </Route>
